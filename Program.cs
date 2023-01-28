bc28205fa79201c44b5433483557163c03e967ab
dianabgs <123804277+dianabgs@users.noreply.github.com>
2023-01-28 14:40:33 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-<<<<<<< Updated upstream
-Console.WriteLine("Line added from feature branch");
-=======
-Console.WriteLine("Hello, World!");
->>>>>>> Stashed changes
+
